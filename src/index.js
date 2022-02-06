8924b53a5774f473e5e7a592c5001d2960d042f1
M.Bashir <muhammadbashir0324@gmail.com>
2022-02-06 17:43:03 +0500

    props branch commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./main.css";
+import ReactDOM from "react-dom";
+function Books() {
+  return (
+    <div className="books">
+      <Booklist
+        author="Bashir"
+        title="Red-Handed: How American Elites Get Rich Helping China Win"
+        img="https://images-na.ssl-images-amazon.com/images/I/81SmH2JFABL._AC_UL254_SR254,254_.jpg" 
+        className="firstCall"
+      >
+      </Booklist>
+      <Booklist
+        author="Abu Huraira"
+        title="
+Things We Never Got Over"
+        img="https://images-na.ssl-images-amazon.com/images/I/71ODaT072wL._UX300__PJku-sticker-v7,TopRight,0,-50_AC_UL254_SR254,254_.jpg"
+      />
+      <Booklist
+        title="Will"
+        author="Sibtain"
+        img="https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg"
+      />
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Booklist(props) {
+    console.log(props);
+  return (
+    <>
+      <div className="myBooks">
+        <img src={props.img} alt="amazon" />
+        <h4>{props.title}</h4>
+        <p>{props.author}</p>
+      </div>
+    </>
+  );
+}
+ReactDOM.render(<Books />, document.getElementById("main"));
